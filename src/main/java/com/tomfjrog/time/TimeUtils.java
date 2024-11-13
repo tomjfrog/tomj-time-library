dab36cb617cede354cd4f13813edd66276714683
tomjfrog <tomj@jfrog.com>
2024-11-13 16:06:33 -0600

    Using Guava methods in codebase

--- src/main/java/com/tomfjrog/time/TimeUtils.java
+++ src/main/java/com/tomfjrog/time/TimeUtils.java
@@ -1,8 +1,12 @@
 package com.tomfjrog.time;
 
+
+
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+// Supplied by the Guava dependency
+import com.google.common.base.Joiner;
 
 public class TimeUtils {
 
@@ -10,6 +14,12 @@ public class TimeUtils {
     public static String getCurrentSystemTime() {
         ZonedDateTime currentTime = ZonedDateTime.now();
         return currentTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z"));
+    }
+
+    public static String reportSupportedZones() {
+        System.out.println("I support TZDB 2005r timezones such as the following:");
+        Joiner joiner = Joiner.on("; ").skipNulls();
+        return joiner.join("CST", null, "MST", "EST");
     }
 
     // Function to return the current system time in a given time zone
